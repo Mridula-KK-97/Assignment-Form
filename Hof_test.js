077fd1dc5734044191e1b300372557867c1da2bb
mridula <mridula8.97@gmail.com>
2025-06-18 16:45:15 +0530

    initial commit

---
+++ Hof_test.js
@@ -0,0 +1,11 @@
+// const numbers=[1,2,3]
+// const doubled=numbers.map(num=>num*2)
+// console.log(doubled)
+
+// const numbers=[1,2,3,4]
+// const evens=numbers.filter(numbers=>numbers%2==0)
+// console.log(evens)
+
+// const numbers=[1,2,3,4,5]
+// const sum=numbers.reduce((i,j)=>i+j)
+// console.log(sum)
